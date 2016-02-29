d184b8c6f584b08135545f1c1d50f0ff5f9cf537
supernoir <kawohl.bentodesign@gmail.com>
2016-02-29 21:01:25 +0100

    creates cards, adds custom sass and fills cards with pseudo content

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+angular.module('heroinesApp', [])
+
+.controller('mainController', function($scope) {
+  $scope.hello = "Hello World!";
+  
+  $scope.card = {
+      'first_name':'Maria',
+      'last_name':'Goeppert Mayer',
+      'nation':'USA',
+      'year':'1963',
+      'discipline':'Physics',      
+  }
+})
