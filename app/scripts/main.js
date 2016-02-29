d67f06e511187a1d0747977a336ea4df83c3dd27
supernoir <kawohl.bentodesign@gmail.com>
2016-02-29 23:28:04 +0100

    adds select input based on faux json source

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,6 +8,17 @@ angular.module('heroinesApp', [])
       'last_name':'Goeppert Mayer',
       'nation':'USA',
       'year':'1963',
-      'discipline':'Physics',      
-  }
+      'discipline':'Physics', 
+      'desc':'The Fox jumps over the Nobel Laureate and he turns green.',      
+  };
+  
+  $scope.disciplines = {
+    'option_1': 'Economics',
+    'option_2': 'Physics',
+    'option_3': 'Chemistry',
+    'option_4': 'Literature',
+    'option_5': 'Peace',
+    'option_6': 'Medicine'
+  };
+  
 })
