040e8004cdf32b6d9f51323fdda7b3813c6125f5
supernoir <kawohl.bentodesign@gmail.com>
2016-03-09 23:13:27 +0100

    adds createYears, createNations

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -22,10 +22,7 @@ heroinesApp.config(function($locationProvider, $routeProvider, $httpProvider) {
 
 heroinesApp.controller('mainController', ['$scope','$http','$location', function($scope, $http, $location) {
 
-var nation = $scope.nation
-$scope.selectNation = function() {
-    console.log("It does something " + nation);
-}
+
 
 $scope.selectDiscipline = function(persons) {
     console.log(persons);
@@ -39,7 +36,41 @@ $scope.selectDiscipline = function(persons) {
        console.log(response.status, "GET Heroines: " + response.statusText);
        console.log(response.data);
        
-       var disciplines = response.data.reduce(function(result, person){
+       var disciplines = createDisciplines(response);
+       var years = createYears(response);
+       var nations = createNations(response);
+
+       
+        $scope.profile = response.data;
+        
+        $scope.disciplines = disciplines;
+        $scope.nations = nations;
+        $scope.years = years;
+        console.log(years);
+        
+        $scope.cards = $scope.profile;
+        
+  }, function errorCallback(response) {
+      console.error(response.status, response.statusText);
+  });
+  
+
+
+$scope.addHeroine = function() {
+    var data = $scope.heroine;
+    console.log(data);
+    $http.post('http://localhost:9001/add_heroine', data).
+        success(function(data) {
+            console.log("Successfully added Heroine");
+        }).error(function(data) {
+            console.error("Error in adding Heroine");
+        })
+    $location.path('/');
+}  
+
+
+function createDisciplines(response) {
+        var disciplines = response.data.reduce(function(result, person){
         var currentDiscipline = person.doc.discipline;
         var currentCategory = null; 
         
@@ -61,34 +92,64 @@ $scope.selectDiscipline = function(persons) {
         currentCategory.persons.push(person);
         return result
        },[]);
-       
-       console.log(disciplines);
-       
-        $scope.profile = response.data;
-        $scope.disciplines = disciplines;
-        $scope.cards = $scope.profile;
+ return disciplines;   
+}
 
+function createYears(response) {
+        var years = response.data.reduce(function(result, person){
+        person.doc.year = parseInt(person.doc.year,10);
+        var currentYear = person.doc.year;
+        var currentCollection = null; 
         
-  }, function errorCallback(response) {
-      console.error(response.status, response.statusText);
-  });
-  
+        for (var i = 0; i < result.length; i++) {
+            var collection = result[i];
+           if (collection.year === currentYear) {
+                currentCollection = collection;
+            break;
+            }
+        }
+        
+        if (!currentCollection) {
+            currentCollection = {
+                year: currentYear,
+                persons: [],
+                }
+                result.push(currentCollection);       
+            }
+        currentCollection.persons.push(person);
+        return result
+       },[]);
+ years = years.sort(function(current, next){
+      return current.year - next.year;
+ });
+ return years;   
+}
 
-
-$scope.addHeroine = function() {
-    var data = $scope.heroine;
-    console.log(data);
-    $http.post('http://localhost:9001/add_heroine', data).
-        success(function(data) {
-            console.log("Successfully added Heroine");
-        }).error(function(data) {
-            console.error("Error in adding Heroine");
-        })
-    $location.path('/');
-}  
-
-
-
+function createNations(response) {
+        var nations = response.data.reduce(function(result, person){
+        var currentNation = person.doc.nation;
+        var currentCollection = null; 
+        
+        for (var i = 0; i < result.length; i++) {
+            var collection = result[i];
+           if (collection.nation === currentNation) {
+                currentCollection = collection;
+            break;
+            }
+        }
+        
+        if (!currentCollection) {
+            currentCollection = {
+                nation: currentNation,
+                persons: [],
+                }
+                result.push(currentCollection);       
+            }
+        currentCollection.persons.push(person);
+        return result
+       },[]);
+ return nations;   
+} 
   
 }]);
 
