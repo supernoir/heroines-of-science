e525b2592ce5ee96fc87e11aedda8d64e33cd85c
supernoir <kawohl.bentodesign@gmail.com>
2016-03-09 22:12:26 +0100

    iterates over the data and displays only selected disciplines

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -27,16 +27,53 @@ $scope.selectNation = function() {
     console.log("It does something " + nation);
 }
 
+$scope.selectDiscipline = function(persons) {
+    console.log(persons);
+    $scope.cards = persons;
+}
+
   $http({
   method: 'GET',
   url: 'http://localhost:9001/heroines'
 }).then(function successCallback(response) {
        console.log(response.status, "GET Heroines: " + response.statusText);
        console.log(response.data);
+       
+       var disciplines = response.data.reduce(function(result, person){
+        var currentDiscipline = person.doc.discipline;
+        var currentCategory = null; 
+        
+        for (var i = 0; i < result.length; i++) {
+            var category = result[i];
+           if (category.discipline === currentDiscipline) {
+                currentCategory = category;
+            break;
+            }
+        }
+        
+        if (!currentCategory) {
+            currentCategory = {
+                discipline: currentDiscipline,
+                persons: [],
+                }
+                result.push(currentCategory);       
+            }
+        currentCategory.persons.push(person);
+        return result
+       },[]);
+       
+       console.log(disciplines);
+       
         $scope.profile = response.data;
+        $scope.disciplines = disciplines;
+        $scope.cards = $scope.profile;
+
+        
   }, function errorCallback(response) {
       console.error(response.status, response.statusText);
-  });  
+  });
+  
+
 
 $scope.addHeroine = function() {
     var data = $scope.heroine;
@@ -51,5 +88,7 @@ $scope.addHeroine = function() {
 }  
 
 
+
   
 }]);
+
